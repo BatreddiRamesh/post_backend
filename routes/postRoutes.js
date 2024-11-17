782defd28b8991fa12700270c0d7fee4f0d8af54
BatreddiRamesh <ramesh.ramesh997@gmail.com>
2024-11-17 11:46:03 +0530

    gfh

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // Setup multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, 'https://github.com/BatreddiRamesh/post_backend/blob/main/uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
